259435bd6873160413500c4857a586ca67fc2643
BurnOutKey <burn.out.wealthy@gmail.com>
2024-10-05 21:03:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/ping", async (req, res) => {
 app.listen(port);
 
 let names = [
-    "https://aaaa8.onrender.com",
+  /*  "https://aaaa8.onrender.com",
     "https://aaaa9.onrender.com",
     "https://aaaa-1-z6h6.onrender.com",
     "https://aaaa-15.onrender.com",
@@ -30,7 +30,96 @@ let names = [
     "https://aaaa3.onrender.com",
     "https://aaaa5.onrender.com",
     "https://aaaa7.onrender.com",
-    "https://aaaa6.onrender.com"
+    "https://aaaa6.onrender.com"*/
+   "cute-coordinated-syzygy",
+            "familiar-busy-snowshoe",
+            "gorgeous-truth-velociraptor",
+            "dune-beryl-turner",
+            "bird-jade-wren",
+            "tattered-pickled-hardware",
+            "perpetual-sky-crowley",
+            "iced-catnip-angolatitan",
+            "silken-easy-bead",
+            "thunder-sequoia-deer",
+            "better-inexpensive-crime",
+            "magic-alive-need",
+            "grass-opalescent-butterfly",
+            "automatic-tulip-veterinarian",
+            "honorable-cloud-silica",
+            "open-honeysuckle-piccolo",
+            "mewing-abstracted-aurora",
+            "cooperative-gentle-change",
+            "plum-buttered-bear",
+            "royal-classy-venom",
+            "brass-famous-damselfly",
+            "south-brook-fur",
+            "charming-dirt-collision",
+            "coral-outrageous-freeze",
+            "expensive-scarce-earth",
+            "sleet-jeweled-humor",
+            "courageous-shocking-chicken",
+            "flaxen-first-motion",
+            "sincere-complete-plier",
+            "watery-just-airplane",
+            "splendid-western-geography",
+            "steep-tundra-braid",
+            "supreme-flicker-leather",
+            "early-cobalt-limit",
+            "temporal-miniature-seeker",
+            "thoughtful-great-rudbeckia",
+            "bronzed-same-munchkin",
+            "walnut-relieved-knuckle",
+            "angry-caterwauling-daughter",
+    "cooperative-humdrum-nerine",
+    "stingy-golden-dragon",
+    "wirehaired-tender-venom",
+    "fluoridated-working-joke",
+    "absorbing-light-archeology",
+    "climbing-scientific-battery",
+    "fuschia-common-cause",
+    "motley-polished-pigment",
+    "handsome-bead-ton",
+    "deadpan-sponge-mall",
+    "splashy-pretty-yarn",
+    "triangular-wary-friction",
+    "billowy-empty-river",
+    "therapeutic-receptive-sweater",
+    "veiled-impartial-erica",
+    "warm-opalescent-fruit",
+    "calico-pear-aphid",
+    "like-freckle-chips",
+    "water-pollen-system",
+    "coal-boiled-enemy",
+    "fan-slash-search",
+    "able-carnelian-gallon",
+    "vaulted-detailed-hardware",
+    "spotty-marvelous-objective",
+    "thundering-lying-lemming",
+    "foil-probable-ring",
+    "curse-working-barge",
+    "glow-puzzling-pyrite",
+    "clover-peach-wool",
+    "tundra-rocky-pediatrician",
+    "cloud-tremendous-carp",
+    "veiled-real-cockroach",
+    "cotton-common-patient",
+    "witty-fish-plantain",
+    "exuberant-beaded-leopard",
+    "quark-zest-fountain",
+    "sapphire-adaptable-slug",
+    "helpful-third-sprite",
+    "midnight-radial-gateway",
+    "bramble-narrow-honey",
+    "general-fresh-chard",
+    "poised-patch-sun",
+    "shade-inky-xenon",
+    "dull-prong-jute",
+    "grape-achieved-sing",
+    "gravel-vast-hip",
+    "fancy-expensive-can",
+    "clever-solid-literature",
+    "judicious-kindly-shell",
+    "brazen-nova-bromine"
 ]
 
 const sendReq = async (direction, amount) => {
@@ -38,15 +127,14 @@ const sendReq = async (direction, amount) => {
 
     const runRequests = () => {
         const promises = names.map(name => {
-            let url = name//`https://${name}.onrender.com/`
-            url+=`/ping?url=${direction}&amount=${amount}`;
+            let url = `https://${name}.glitch.me/ping?url=${direction}&amount=${amount}`;
 
             total.amount += amount;
-console.log(url)
+
             return fetch(url)
                 .then(res => res.text())
                 .then(text => {
-                    let parts = text.split("/");
+                    let parts = text.split("?");
                     let [ failed, success ] = [Number(parts[0]), Number(parts[1].split("/")[0])]
 console.log(text)
                     total.failed += failed;
