2ab0bc64103cdf2cd7cc34ba300273d5525a82ff
BurnOutKey <burn.out.wealthy@gmail.com>
2024-10-05 20:38:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,69 @@
+const fetch = require("node-fetch");
+
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3000;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use(express.json());
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/ping", async (req, res) => {
+  return res.send(`alive`);
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.listen(port);
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+let names = [
+    "https://aaaa8.onrender.com",
+    "https://aaaa9.onrender.com",
+    "https://aaaa-1-z6h6.onrender.com",
+    "https://aaaa-15.onrender.com",
+    "https://aaaa-16.onrender.com",
+    "https://aaaa-17.onrender.com",
+    "https://aaaa-18.onrender.com",
+    "https://aaaa-12.onrender.com",
+    "https://lol-d399.onrender.com",
+    "https://aaaa-j6u9.onrender.com",
+    "https://aaaa1.onrender.com",
+    "https://aaaa2.onrender.com",
+    "https://aaaa4.onrender.com",
+    "https://aaaa3.onrender.com",
+    "https://aaaa5.onrender.com",
+    "https://aaaa7.onrender.com",
+    "https://aaaa6.onrender.com"
+]
+
+const sendReq = async (direction, amount) => {
+    let total = { failed: 0, success: 0, amount: 0 };
+
+    const runRequests = () => {
+        const promises = names.map(name => {
+            let url = name//`https://${name}.onrender.com/`
+            url+=`/ping?url=${direction}&amount=${amount}`;
+
+            total.amount += amount;
+console.log(url)
+            return fetch(url)
+                .then(res => res.text())
+                .then(text => {
+                    let parts = text.split("/");
+                    let [ failed, success ] = [Number(parts[0]), Number(parts[1].split("/")[0])]
+console.log(text)
+                    total.failed += failed;
+                    total.success += success;
+                })
+                .catch(e => {
+           // console.log(name, "failed")
+            });
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+
+        return Promise.all(promises);
+    };
+
+    await runRequests();
+
+    console.log(`${total.success}/${total.amount}`);
+};
+
+setInterval(() => {
+ sendReq(`https://api.moomoo.io/servers?v=1.22`, 35);
+}, 70e3);
